6ed6afcc4b7358ac35c737528d34724d0bab53e8
Your Name <leonardongc@gmail.com>
2022-06-04 18:21:10 -0300

    almost done

---
+++ Base62.py
@@ -0,0 +1,16 @@
+from base64 import encode
+from string import ascii_letters, digits
+
+base62 = digits + ascii_letters
+base62Size = len(base62)
+
+def encodeBase62(number, encoded=''):
+    return encodeBase62(number//base62Size, base62[number%base62Size] + encoded) if number >= base62Size else base62[number%base62Size] + encoded
+
+def decodeBase62(text,decoded=0):
+    return decodeBase62(text[1:], decoded*base62Size + base62.index(text[0])) if text else decoded
+
+def search4patterns(salt,patterns2find):
+    try:
+
+    except:
