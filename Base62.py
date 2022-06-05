35a44efba1225a7d3b31b0e7cb5d31432a25d7a3
Your Name <leonardongc@gmail.com>
2022-06-05 15:23:33 -0300

    using sha256 to generate pseudorandom codes

--- Base62.py
+++ Base62.py
@@ -1,19 +1,28 @@
 from base64 import encode
 from string import ascii_letters, digits
+from hashlib import sha256
 
 base62 = digits + ascii_letters
 base62Size = len(base62)
 
-def encodeBase62(number, encoded=''):
+def encodeBase62(number, encoded = ''):
     return encodeBase62(number//base62Size, base62[number%base62Size] + encoded) if number >= base62Size else base62[number%base62Size] + encoded
 
-def decodeBase62(text,decoded=0):
+def decodeBase62(text,decoded = 0):
     return decodeBase62(text[1:], decoded*base62Size + base62.index(text[0])) if text else decoded
 
+def generateFixedLenghtCode(targetLenght, seed = ''):
+    if targetLenght < 1:
+        raise('Lenght must be a non null positive number')
 
-"""
-def search4patterns(salt,patterns2find):
-    try:
+    overMaximum = base62Size**targetLenght
+    minimum = base62Size**(targetLenght -1)   
+    baseHash = int(sha256(seed.encode()).hexdigest(),16)
+    #make it better!!!!!!
+    if baseHash >= overMaximum:
+        baseHash = int(((baseHash**(-1))+(overMaximum**(-1)))**(-1))
 
-    except:
-"""
+    if baseHash < minimum:
+        baseHash += minimum
+
+    return encodeBase62(baseHash)
